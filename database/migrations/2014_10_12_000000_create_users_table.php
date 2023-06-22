a8da86a453b5222e480db178292806e92c0fbefb
dennis <a0981038610@gmail.com>
2023-06-22 23:06:02 +0800

    add jwt

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->comment('管理員');
         });
     }
 
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('register', [Controllers\AuthController::class, 'register']);
+    Route::post('login', [Controllers\AuthController::class, 'login']);
+    Route::get('refresh', [Controllers\AuthController::class, 'refresh']);
+    Route::get('user', [Controllers\AuthController::class, 'user']);
+    Route::get('logout', [Controllers\AuthController::class, 'logout']);
 });
